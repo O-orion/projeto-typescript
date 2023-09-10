dc6741951640d67b72102afc71d9a7bd2af28289
Luca Viana <78035167+O-orion@users.noreply.github.com>
2023-09-10 11:28:22 -0300

    add enum e types alias

---
+++ dist/js/typs.js
@@ -0,0 +1,5 @@
+let valor = 3000; // boas praticas é declara o tipo da variavel.
+valor = Number('ll');
+let nome = '';
+let isPago = false;
+let qualquer = '';
--- src/bytebank.ts
+++ src/bytebank.ts
@@ -1,5 +1,6 @@
 let saldo = 3000;
 
+alert('olá')
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
 if (elementoSaldo != null) {
     elementoSaldo.textContent = saldo.toString();
