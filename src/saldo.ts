36044a93e370a3d557880757cccdfac0dbda5049
Luca Viana <78035167+O-orion@users.noreply.github.com>
2023-09-11 22:36:06 -0300

    organizando codigo

--- src/saldo.ts
+++ src/saldo.ts
@@ -1,7 +1,25 @@
 let saldo = 3000;
 
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
+const elementoDataAcesso = document.querySelector(".block-saldo time") as HTMLElement;
+
 if (elementoSaldo != null) {
-    elementoSaldo.textContent = saldo.toString();
+    /* Formatando o saldo no estilo de moeda, moeda real brasileira
+    elementoSaldo.textContent = saldo.toLocaleString('pt-br', {currency: 'BRL', style: 'currency'}); */
+    elementoSaldo.textContent = formatarMoeda(saldo)
 }
 
+
+if (elementoDataAcesso != null) {
+    const dataAcesso = new Date();
+    elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO)
+    /*
+    elementoDataAcesso.textContent = dataAcesso.toLocaleDateString("pt-br",
+    {
+        weekday: 'long', //formato longo de exibição
+        day: '2-digit', // no formato de dois digitos
+        month: '2-digit', // no formato de dois digitos
+        year: 'numeric', // número total do ano
+    }) // Configurando a data 
+    */
+}
